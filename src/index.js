0a54ff396945eda7b4e02ba7e62a3e82d8780e40
Jordan Kline <jordan.kline@gmail.com>
2021-10-20 16:13:09 -0700

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const express = require("express");
+const http = require("http");
+
+const { URL, PORT } = process.env;
+
+const app = express();
+const server = http.createServer(app);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  socket.on("MESSAGE", (msg) => {
+    io.emit("MESSAGE", msg);
+  });
+
+  socket.on("SEND_JOIN_REQUEST", (msg) => {
+    io.emit("JOIN_REQUEST_ACCEPTED");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on *:${PORT}`);
+});
